5dbc18683174e0bd1ff4ff55ab4ce70d66138850
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-02-23 11:27:32 +0530

    :: connection and listen

--- backend_project/src/index.js
+++ backend_project/src/index.js
@@ -1,1 +1,29 @@
-console.log("Project Setup");
+
+import dotenv from 'dotenv'
+dotenv.config({
+    path:'./.env'
+})
+import express from "express"
+import connectDB from './db/index.js'
+
+const app = express()
+
+connectDB();
+// ( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}`)
+//         console.log("DB is connected")
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+
+//     } catch (error) {
+//         console.error("ERROR: ", error)
+//         throw new err
+//     }
+// })();
+
+app.listen(process.env.PORT, () => {
+        console.log(`App is listening on port ${process.env.PORT}`);
+})
