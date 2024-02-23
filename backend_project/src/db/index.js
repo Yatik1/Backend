15480075f92a8580a5898c5a5d082951f26ab5e6
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-02-23 11:27:08 +0530

    mongodb

---
+++ backend_project/src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const connectDB = async () => {
+    try {
+        const db = await mongoose.connect(`${process.env.MONGODB_URL}`)
+        console.log(`Database is connected on : ${db.connection.host}`)
+    } catch (error) {
+        console.log("Error : " , error)
+        process.exit(1)
+    }
+}
+
+ export default connectDB
