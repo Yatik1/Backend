727b0a575cbeff4024dda380e65ac3caceafc8e1
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-02-24 10:27:02 +0530

    :: debugg

--- backend_project/src/db/index.js
+++ backend_project/src/db/index.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose"
 
 const connectDB = async () => {
-    try {
-        const db = await mongoose.connect(`${process.env.MONGODB_URL}`)
+    try { 
+        const db = await mongoose.connect(process.env.MONGODB_URL || 8000)
         console.log(`Database is connected on : ${db.connection.host}`)
     } catch (error) {
         console.log("Error : " , error)
