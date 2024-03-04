04ef8323f09a21f0e531ea7c48b2e0f726b40167
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-03-04 19:42:22 +0530

    cloud utility

--- backend_project/src/utils/cloudinary.js
+++ backend_project/src/utils/cloudinary.js
@@ -1,12 +1,23 @@
 import {v2 as cloudinary} from 'cloudinary';
 import fs from "fs"
 
+import dotenv from 'dotenv'
+dotenv.config({
+    path:'./.env'
+})
+
 
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key:process.env.CLOUDINARY_API_KEY, 
   api_secret:process.env.CLOUDINARY_API_SECRET 
 });
+
+// cloudinary.config({ 
+//     cloud_name: 'dxzacmrj0', 
+//     api_key: '154635499739836', 
+//     api_secret: "rzpxJEWA49donLF8hXCMG4UCkUU" 
+//   });
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
@@ -16,8 +27,11 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type:'auto'
         })
         console.log("file is uploaded on cloudinary" , response.url);
+        fs.unlinkSync(localFilePath) 
+        return response;
     } catch (error) {
-         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+         fs.unlinkSync(localFilePath) 
+         console.log(error); // remove the locally saved temporary file as the upload operation got failed
          return null;
     }
 }
