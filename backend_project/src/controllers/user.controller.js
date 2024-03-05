ac4f68551f8a0328094e434bdd4a9f8ec1de6581
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-03-05 19:30:25 +0530

    :: controller

--- backend_project/src/controllers/user.controller.js
+++ backend_project/src/controllers/user.controller.js
@@ -112,8 +112,47 @@ const loginUser = asyncHandler(async (req,res) => {
     const isPassword = await user.isPasswordCorrect(password)
     if(!isPassword) throw new ApiError(401 , "Invalide user credentials");
 
-    
+   const {accessToken  , refreshToken} =  await generateAccessAndRefreshToken(user._id)
+   
+   const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+   const options = {
+      httpOnly:true,
+      secure:true
+   }
+
+   return res.status(200)
+              .cookie("accessToken" , accessToken , options)
+              .cookie("refreshToken" , refreshToken , options)
+               .json(
+                new ApiResponse(200 , {
+                    user: loggedInUser , accessToken,refreshToken
+                } ,"User logged In Successfully" 
+                )
+               )
 
 })
 
-export {registerUser}
+const logoutUser = asyncHandler(async (req,res) => {
+    await User.findByIdAndUpdate(
+        req.user._id , {
+            $set: {
+                refreshToken : undefined
+            }
+        } , {
+            new:true
+        } 
+    )
+
+    const options = {
+        httpOnly:true,
+        secure:true
+     }
+
+    return res.status(200)
+               .clearCookie("accessToken" , options)
+               .clearCookie("refreshToken" , options)
+                .json(new ApiResponse(200 , {} , "User logged out "))
+})
+
+export {registerUser , loginUser , logoutUser}
