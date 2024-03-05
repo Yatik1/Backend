451e23107b24e9411af8e3bf58f1d71727efd8e8
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-03-05 12:06:20 +0530

    env

--- backend_project/src/utils/cloudinary.js
+++ backend_project/src/utils/cloudinary.js
@@ -13,11 +13,7 @@ cloudinary.config({
   api_secret:process.env.CLOUDINARY_API_SECRET 
 });
 
-// cloudinary.config({ 
-//     cloud_name: 'dxzacmrj0', 
-//     api_key: '154635499739836', 
-//     api_secret: "rzpxJEWA49donLF8hXCMG4UCkUU" 
-//   });
+
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
